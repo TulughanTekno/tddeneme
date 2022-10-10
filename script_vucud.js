91d953f96f4a3254050db8db3f85f36e89da4e81
TulughanTekno <76101129+TulughanTekno@users.noreply.github.com>
2022-10-10 05:56:52 +0300

    bitti

--- script_vucud.js
+++ script_vucud.js
@@ -41,57 +41,198 @@ elementvn.addEventListener("click", vn);
 
 function va() {
   document.getElementById("demo_g").innerHTML = 'das Haar';
+  playAudio("seskorper/das haar.mp3")
 }
 
 function vb() {
-  document.getElementById("demo_g").innerHTML = 'der Stirn';
+  document.getElementById("demo_g").innerHTML = 'die Stirn';
+  playAudio("seskorper/die stirn.mp3")
 }
 
 function vc() {
     document.getElementById("demo_g").innerHTML = 'der Mund';
+    playAudio("seskorper/der mund.mp3")
   }
   
 function vd() {
     document.getElementById("demo_g").innerHTML = 'der Kopft';
+    playAudio("seskorper/der kopf.mp3")
   }
 
 function ve() {
     document.getElementById("demo_g").innerHTML = 'die Schulter';
+    playAudio("seskorper/die shulter.mp3")
   }
   
   function vf() {
     document.getElementById("demo_g").innerHTML = 'der Ellbogen';
+    playAudio("seskorper/der ellbogen.mp3")
   }
   
   function vg() {
       document.getElementById("demo_g").innerHTML = 'der Arm';
+      playAudio("seskorper/der arm.mp3")
     }
     
     function vh() {
       document.getElementById("demo_g").innerHTML = 'das Handgelenk';
+      playAudio("seskorper/das handgelenk.mp3")
     }
   
     function vi() {
       document.getElementById("demo_g").innerHTML = 'die Hand';
+      playAudio("seskorper/die hand.mp3")
     }
     
     function vj() {
       document.getElementById("demo_g").innerHTML = 'der Bauch';
+      playAudio("seskorper/der bauh.mp3")
     }
     
     function vk() {
         document.getElementById("demo_g").innerHTML = 'das Knie';
+        playAudio("seskorper/das knie.mp3")
       }
       
       function vl() {
         document.getElementById("demo_g").innerHTML = 'das Fußgelenk';
+        playAudio("seskorper/das fussgelenk.mp3")
       }
   function vn() {
     document.getElementById("demo_g").innerHTML = 'der Fuß';
+    playAudio("seskorper/der Fuß.mp3")
  }
       
+ const elementya = document.getElementById("y_a");
+ elementya.addEventListener("click", va);
+ 
+ const elementyb = document.getElementById("y_b");
+ elementyb.addEventListener("click", vb);
+ 
+ const elementyc = document.getElementById("y_c");
+ elementyc.addEventListener("click", vc);
+ 
+ const elementye = document.getElementById("y_e");
+ elementye.addEventListener("click", vd);
+ 
+ const elementydl = document.getElementById("y_dl");
+ const elementydr = document.getElementById("y_dr");
+ elementydl.addEventListener("click", ve);
+ elementydr.addEventListener("click", ve);
+ 
+ const elementyf = document.getElementById("y_f");
+ elementyf.addEventListener("click", vf);
+ 
+ const elementyg = document.getElementById("y_g");
+ elementyg.addEventListener("click", vg);
+ 
+ const elementyh = document.getElementById("y_h");
+ elementyh.addEventListener("click", vh);
+ 
+ const elementyi = document.getElementById("y_i");
+ elementyi.addEventListener("click", vi);
+ 
+ function ya() {
+  document.getElementById("demo_y").innerHTML = 'das Haar';
+  playAudio("seskorper/das haar.mp3")
+}
+
+function yb() {
+  document.getElementById("demo_y").innerHTML = 'das Ohr';
+  playAudio("seskorper/das ohr.mp3")
+}
+
+function yc() {
+    document.getElementById("demo_y").innerHTML = 'das Auge';
+    playAudio("seskorper/das auge.mp3")
+  }
+  
+function yd() {
+    document.getElementById("demo_y").innerHTML = 'die Augenbraue';
+    playAudio("seskorper/die augenbraue.mp3")
+  }
+
+function ye() {
+    document.getElementById("demo_y").innerHTML = 'die Nase';
+    playAudio("seskorper/die nase.mp3")
+  }
+  
+  function yf() {
+    document.getElementById("demo_y").innerHTML = 'die Wange';
+    playAudio("seskorper/die wange.mp3")
+  }
+  
+  function yg() {
+      document.getElementById("demo_y").innerHTML = 'der Zahn';
+      playAudio("seskorper/der zahn.mp3")
+    }
+    
+    function yh() {
+      document.getElementById("demo_y").innerHTML = 'die Lippe';
+      playAudio("seskorper/die lippe.mp3")
+    }
+  
+    function yi() {
+      document.getElementById("demo_y").innerHTML = 'die Stirn';
+      playAudio("seskorper/die stirn.mp3")
+    }
+    
 
 
+
+    const elementea = document.getElementById("e_a");
+    elementea.addEventListener("click", ea);
+    
+    const elementeb = document.getElementById("e_b");
+    elementeb.addEventListener("click", eb);
+    
+    const elementec = document.getElementById("e_c");
+    elementec.addEventListener("click", ec);
+    
+    const elementee = document.getElementById("e_e");
+    elemented.addEventListener("click", vd);
+    
+    const elementyefa = document.getElementById("e_fa");
+    const elementyefb = document.getElementById("e_fb");
+    elementefa.addEventListener("click", ve);
+    elementefb.addEventListener("click", ve);
+    
+    const elementefc = document.getElementById("e_fc");
+    elementefc.addEventListener("click", vf);
+    const elementefd = document.getElementById("e_fd");
+    elementefd.addEventListener("click", vf);
+    const elementefe = document.getElementById("e_fe");
+    elementefe.addEventListener("click", vf);
+
+    function ea() {
+      document.getElementById("demo_e").innerHTML = 'der Kleinefinger';
+      playAudio("sesparmak/serçe.mp3")
+    }
+    
+    function eb() {
+      document.getElementById("demo_e").innerHTML = 'der Ringfinger';
+      playAudio("sesparmak/yüzük.mp3")
+    }
+    
+    function ec() {
+        document.getElementById("demo_e").innerHTML = 'der Mittelfinger';
+        playAudio("sesparmak/orta.mp3")
+      }
+      
+    function ed() {
+        document.getElementById("demo_e").innerHTML = 'der Zeigefinger';
+        playAudio("sesparmak/işaret.mp3")
+      }
+    
+    function ef() {
+        document.getElementById("demo_e").innerHTML = 'der Nagel';
+        playAudio("seskorper/der nagel.mp3")
+      }
+
+      function ee() {
+        document.getElementById("demo_e").innerHTML = 'der Daumen';
+        playAudio("sesparmak/der daumen.mp3")
+      }
 
   function playAudio(url) {
     new Audio(url).play();
