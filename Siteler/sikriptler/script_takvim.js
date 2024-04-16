9bc77f78b4880cf12546fd5ef2ff87a36bb92021
TulughanTekno <tulu20062008@gmail.com>
2024-04-16 15:31:15 +0300

    Takvim sesleri eklendi
    +orjinal dosyaların yeri değişti
    +takvim sesleri eklendi
    +Yüz kayma düzeltildi

--- Siteler/sikriptler/script_takvim.js
+++ Siteler/sikriptler/script_takvim.js
@@ -10,25 +10,31 @@ elementmsa.addEventListener("click", msc);
 const elementmsd = document.getElementById("ms_d");
 elementmsb.addEventListener("click", msd);
 
+function playAudio(url) {
+  new Audio(url).play();
+}
+
 
 function msa() {
   document.getElementById("demo").innerHTML = 'der Frühling';
+  playAudio("Sesler/takvimses/frühling.mp3")
 }
 
 function msb() {
   document.getElementById("demo").innerHTML = 'der Sommer';
+  playAudio("Sesler/takvimses/sommer.mp3")
 }
 
 function msc() {
     document.getElementById("demo").innerHTML = 'der Winter';
+    playAudio("Sesler/takvimses/winter.mp3")
   }
   
   function msd() {
     document.getElementById("demo").innerHTML = 'der Herbst';
+    playAudio("Sesler/takvimses/herbst.mp3")
   }
 
 
 
-  function playAudio(url) {
-    new Audio(url).play();
-  }
+  
